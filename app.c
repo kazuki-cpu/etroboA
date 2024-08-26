c61bab3c87f88683d2a5be2caa129ea8f2043b72
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-26 20:31:08 +0900

    Update app.c

--- app.c
+++ app.c
@@ -60,7 +60,7 @@ static void laptime_tracer(){
     /* 計測器初期化 */
     odom_Distance_reset();
     //odom_Direction_reset();
-    odom_Direction_setDirection();
+    odom_Direction_setDirection(30);
     sta_cyc(TRACER_TASK_CYC);  /* トレースタスクの起動 */
     sta_cyc(ODOMETRY_TASK_CYC);  /*オドメトリタスクの起動 */
 }
