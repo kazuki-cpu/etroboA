fe0f03cb7ae5389d5837247ebdebef94930cf976
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-09-03 21:20:02 +0900

    Update Odometry.c

--- Odometry/Odometry.c
+++ Odometry/Odometry.c
@@ -172,11 +172,13 @@ void odom_Direction_resetSync(float handover_dir){
     //一旦オドメトリタスクをストップ&待ち
     stp_cyc(ODOMETRY_TASK_CYC);
     wait_msec(50);
+    
     odom_Direction_reset();
     wait_msec(100);
 
     //最後の方位を代入＆オドメトリタスク再開
-    odom_Direction_setDirection(handover_dir);
     sta_cyc(ODOMETRY_TASK_CYC);
+    odom_Direction_setDirection(handover_dir);
+    wait_msec(50);
 }
 
