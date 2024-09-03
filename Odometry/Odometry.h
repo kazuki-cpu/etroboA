b4345b21ef3502133be98096f972347aa8057b29
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-09-03 18:17:59 +0900

    Update Odometry.h

--- Odometry/Odometry.h
+++ Odometry/Odometry.h
@@ -31,7 +31,7 @@ typedef struct{
   void odom_Coordinate_reset();
   void odom_Coordinate_getCoordinate();
   static ODOM_XY odom_Coordinate_update();
-  void odom_Direction_setCoordinate(float set_x, float set_y);
+  void odom_Coordinate_setXY(float set_x, float set_y);
   void odom_Distance_resetSync(float handover_dir);
   void odom_Direction_resetSync(float handover_dir);
 
