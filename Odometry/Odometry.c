6ed9c3c7c8e55fb60498a9d38629e11c3c354982
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-09-03 20:42:22 +0900

    Update Odometry.c

--- Odometry/Odometry.c
+++ Odometry/Odometry.c
@@ -106,7 +106,10 @@ float odom_Direction_getDirection(){
 /* 方位を更新 */
 void odom_Direction_update(){
     //(360 / (2 * 円周率 * 車体トレッド幅)) * (右進行距離 - 左進行距離)
-    direction += (360.0 / (2.0 * PI * TREAD)) * (odom_Distance_getDistanceRight() - odom_Distance_getDistanceLeft());
+    //direction += (360.0 / (2.0 * PI * TREAD)) * (odom_Distance_getDistanceRight() - odom_Distance_getDistanceLeft());
+    direction_rad += (1 / TREAD) * (odom_Distance_getDistanceRight() - odom_Distance_getDistanceLeft());
+    direction_deg = (180 / PI) * direction_rad;
+    
 }
 
 /* 方位を設定 */
