c312c8d5c0955e2fafe736ac220f380c0df3d30e
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-09-04 23:32:15 +0900

    Update app.c

--- app.c
+++ app.c
@@ -1,40 +1,29 @@
-#pragma once
-
 #include <stdio.h>
 #include "app.h"
 #include "Calibration.h"
-#include "Odometry.h"
+#include "Manager.h"
 
-/* グローバル変数 */
+/***************************************************************************************************
+    タスク定義
+ ***************************************************************************************************/
 
 /* メインタスク(起動時にのみ関数コールされる) */
 void main_task(intptr_t unused){
-    //init_global();                  /* グローバル変数の初期化 */
-    port_config();                  /* ポート初期設定 */
-    wait_start();                   /* 動作開始待ち（シミュレータ：自動またはスペースキーで開始, 実機：BlueToothボタンで開始） */
+    port_config();          /* ポート初期設定 */
+    wait_start();           /* 動作開始待ち（シミュレータ：自動で開始, 実機：BlueToothボタンで開始） */
     wait_msec(1000);
     calibration(false);     /* キャリブレーション（true:実行、false:スキップ） */
     wait_msec(1000);
-    laptime_tracer();               /* ラップタイム走行 */
-                                    /* タブルループNEO */
-                                    /* スマートデブリ */
-                                    /* スマートキャリー */
-                                    /* 停車処理 */
-    ext_tsk();                      /* メインタスク終了 */
+    start_manager(TRACER);   /* 競技開始（開始する競技を指定：TRACER,DEBRI） */
+    ext_tsk();              /* メインタスク終了 */
 }
 
-/* グローバル変数の初期化 */
-/*static void init_global(){
-    s_min = 255;
-    s_max = 0;
-    s_ave = 90;
-    v_min = 255;
-    v_max = 0;
-    v_ave = 50;
-}*/
+/***************************************************************************************************
+    メインタスク用の関数
+ ***************************************************************************************************/
 
 /* ポート初期設定 */
-static void port_config(){
+void port_config(){
     /* センサー入力ポートの設定 */
     ev3_sensor_config(touch_sensor ,TOUCH_SENSOR);
     ev3_sensor_config(color_sensor ,COLOR_SENSOR);
@@ -56,18 +45,13 @@ void wait_start(){
             break;
         }
         wait_msec(50);
+        printf("*");
     }
 }
 
-/* ラップタイム走行 */
-static void laptime_tracer(){
-    /* 計測器初期化 */
-    odom_Distance_reset();
-    //odom_Direction_reset();
-    odom_Direction_setDirection(0.0);
-    sta_cyc(TRACER_TASK_CYC);  /* トレースタスクの起動 */
-    sta_cyc(ODOMETRY_TASK_CYC);  /*オドメトリタスクの起動 */
-}
+/***************************************************************************************************
+    共通関数
+ ***************************************************************************************************/
 
 /* タスク停止関数（ミリ秒） */
 void wait_msec(int32_t msec){
